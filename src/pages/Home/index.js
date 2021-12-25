89b8ea9f333e2c06a9e22b226d8db5f0d7fee83b
Mohammadreza <Mrhhajiyan@gmail.com>
2021-12-26 03:23:40 +0330

    fixed home with useCart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,10 @@
-import { useRecoilState } from "recoil";
 import { useState, useEffect } from "react";
-import { cartState, KEY_CART } from "atom/cart";
 import styles from "./style.module.css";
+import useCart from "recoil-utils";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useRecoilState(cartState);
+  const [cart, setCart] = useCart();
   const fetchProducts = async () => {
     const getData = await fetch("https://fakestoreapi.com/products");
     const data = await getData.json();
@@ -26,27 +25,28 @@ const Home = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
-  useEffect(() => {
-    localStorage.setItem(KEY_CART, JSON.stringify(cart));
-  }, [cart]);
   return (
     <>
-      <h2 className="text-center">Products</h2>
+      <h2 className="text-center mt-3">Products</h2>
       <div className={styles.wrapperProducts}>
-        {products.map((item) => (
-          <div key={item.id}>
-            <div className={styles.wrapImg}>
-              <img src={item.image} alt={item.title} />
+        {products.length > 0 ? (
+          products.map((item) => (
+            <div key={item.id}>
+              <div className={styles.wrapImg}>
+                <img src={item.image} alt={item.title} />
+              </div>
+              <div className={styles.content}>
+                <h2>{item.title.slice(0, 20)}</h2>
+                <p>{item.description.slice(0, 70)}</p>
+                <button onClick={() => addToCartHandler(item)}>
+                  Add to Cart
+                </button>
+              </div>
             </div>
-            <div className={styles.content}>
-              <h2>{item.title.slice(0, 20)}</h2>
-              <p>{item.description.slice(0, 70)}</p>
-              <button onClick={() => addToCartHandler(item)}>
-                Add to Cart
-              </button>
-            </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </>
   );
